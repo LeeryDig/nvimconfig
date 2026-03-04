013c713b0e4fc5e3dd842af91b70a9e444662ed2
LeeryDig <guigacgui99@gmail.com>
2026-03-04 10:53:29 -0300

    config update

--- init.lua
+++ init.lua
@@ -1,6 +1,42 @@
 -- Leader key
 vim.g.mapleader = " "
 
+-- NETRW BASE
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+vim.g.netrw_browse_split = 4
+vim.g.netrw_winsize = 25
+
+-- Toggle lateral direita
+local function toggle_netrw()
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    local buf = vim.api.nvim_win_get_buf(win)
+    local ft = vim.api.nvim_buf_get_option(buf, "filetype")
+    if ft == "netrw" then
+      vim.api.nvim_win_close(win, true)
+      return
+    end
+  end
+
+  vim.cmd("botright vsplit")
+  vim.cmd("vertical resize 30")
+  vim.cmd("Ex")
+end
+
+vim.keymap.set("n", "<leader>e", toggle_netrw, { silent = true })
+
+-- AUTO LAYOUT QUANDO ABRIR COM nvim .
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.isdirectory(vim.fn.argv(0)) == 1 then
+      vim.cmd("enew")              -- cria buffer vazio principal
+      vim.cmd("botright vsplit")
+      vim.cmd("vertical resize 30")
+      vim.cmd("Ex")
+    end
+  end,
+})
+
 -- Terminal horizontal (embaixo)
 vim.keymap.set("n", "<leader>th", function()
   vim.cmd("split")
@@ -16,21 +52,21 @@ end)
 -- Sair do modo terminal com ESC
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
 
--- Redimensionar splits com Ctrl + setas
-vim.keymap.set("n", "<C-Up>", ":resize +2<CR>")
-vim.keymap.set("n", "<C-Down>", ":resize -2<CR>")
-vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>")
-vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>")
+-- Redimensionar splits com Alt + setas
+vim.keymap.set("n", "<A-Up>", ":resize +2<CR>")
+vim.keymap.set("n", "<A-Down>", ":resize -2<CR>")
+vim.keymap.set("n", "<A-Right>", ":vertical resize +2<CR>")
+vim.keymap.set("n", "<A-Left>", ":vertical resize -2<CR>")
 
 -- Mostrar número de linhas
  vim.opt.number = true          -- número absoluto
 -- vim.opt.relativenumber = true  -- número relativo (ótimo pra pular linhas)
 
 -- Manter margem ao rolar
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 16
 
 -- linhas acima/abaixo do cursor
-vim.opt.sidescrolloff = 10
+vim.opt.sidescrolloff = 16
 
 vim.api.nvim_create_autocmd("TermOpen", {
   callback = function()
